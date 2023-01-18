7d8000bca962ffc3d28d0e9ab8417c170466a2d7
edzusk <edzus.krisjanis@gmail.com>
2023-01-18 18:58:37 +0200

    MD_08_v01

--- 08_MD_Memory_Game_(No frameworks)/src/scripts.ts
+++ 08_MD_Memory_Game_(No frameworks)/src/scripts.ts
@@ -2,9 +2,16 @@ console.log('Ready for coding');
 
 const levelSelector = document.querySelectorAll<HTMLInputElement>('.btn-level');
 const gameField = document.querySelector('.gameField');
-const getCards = () => document.querySelectorAll('.card');
 const reset = document.querySelector('.reset');
+// const timer = document.querySelector('.timer');
+const getCards = () => document.querySelectorAll('.card');
+const getFaces = () => document.querySelectorAll('.face');
+const getBacks = () => document.querySelectorAll('.back');
 let cards = getCards();
+let faces = getFaces();
+let backs = getBacks();
+// let gameStop = false;
+
 
 const cardBacks:{[key:string]:string} = {
   boy: './assets/images/boy.png',
@@ -21,96 +28,155 @@ const cardBacks:{[key:string]:string} = {
   sloth: './assets/images/sloth.png',
 };
 const cardFace = './assets/images/card_face.png';
-const keys: string[] = Object.keys(cardBacks);
+const cardBackKeys: string[] = Object.keys(cardBacks);
 let fieldSize = 0;
-let randomkeys: number[] = [];
-let counter = 0;
-let card1 = '';
-let card1Num: number;
-let card2 = '';
-let card2Num: number;
+let randomKeyNumbers: number[] = [];
+let openedCards = 0;
+let firstOpenedCardValue = '';
+let firstOpenedCardIndex: number;
+let secondOpenedCardValue = '';
+let secondOpenedCardIndex: number;
 let guessedCards: number[] = [];
-const getRandomkeys = () => {
+let points = 0;
+// let time = setInterval(() => (0));
+// let currentTime = 0;
+const getRandomKeyNumbers = () => {
   for (let i = 0; i < fieldSize / 2; i += 1) {
-    const randnum = Math.floor(Math.random() * keys.length);
-    if (randomkeys.includes(randnum)) {
+    const randnum = Math.floor(Math.random() * cardBackKeys.length);
+    if (randomKeyNumbers.includes(randnum)) {
       i -= 1;
     } else {
-      randomkeys.push(randnum);
-      randomkeys.push(randnum);
+      randomKeyNumbers.push(randnum);
+      randomKeyNumbers.push(randnum);
     }
   }
-  randomkeys = randomkeys.sort(() => 0.5 - Math.random());
+  randomKeyNumbers = randomKeyNumbers.sort(() => 0.5 - Math.random());
 };
-const winCheck = ()=> {
-    
-}
+
 reset.addEventListener('click', () => {
   document.querySelector('.levelSelector').classList.remove('hidden');
   document.querySelector('.gameField').classList.add('hidden');
+  document.querySelector('.win').classList.add('hidden');
   gameField.innerHTML = '';
   fieldSize = 0;
-  randomkeys = [];
-  counter = 0;
-  card1 = '';
-  card1Num = NaN;
-  card2 = '';
-  card2Num = NaN;
+  randomKeyNumbers = [];
+  openedCards = 0;
+  firstOpenedCardValue = '';
+  firstOpenedCardIndex = NaN;
+  secondOpenedCardValue = '';
+  secondOpenedCardIndex = NaN;
   guessedCards = [];
+  // gameStop = true;
 });
 const hideShow = () => {
   document.querySelector('.levelSelector').classList.add('hidden');
   document.querySelector('.gameField').classList.remove('hidden');
 };
+const flipCard = (ind:number) => {
+  faces = getFaces();
+  backs = getBacks();
+  faces[ind].classList.add('hidden');
+  backs[ind].classList.remove('hidden');
+};
+const flipCardBack = (ind:number) => {
+  faces = getFaces();
+  backs = getBacks();
+  faces[ind].classList.remove('hidden');
+  backs[ind].classList.add('hidden');
+};
 
-const resetCardFaces = () => {
+const hideWrongGuess = () => {
   cards.forEach((el, i) => {
     if (!guessedCards.includes(i)) {
-      el.innerHTML = `<img class="wrapper" src="${cardFace}">`;
+      flipCardBack(i);
     }
   });
 };
 
 const drawFiled = () => {
   for (let i = 0; i < fieldSize; i += 1) {
-    gameField.innerHTML += `<div class="card"><img class="wrapper" src="${cardFace}" alt="parrot"></div>`;
+    gameField.innerHTML += `<div class="card"><img class="wrapper back hidden" src="${cardBacks[cardBackKeys[randomKeyNumbers[i]]]}"><img class="wrapper face" src="${cardFace}" alt="parrot"></div>`;
   }
 };
+
+// const startTimer = () => {
+//   let i = 0;
+//   clearInterval(time);
+//   time = setInterval(() => {
+//     if (i <= 999) {
+//       currentTime = i;
+//       i += 1;
+//     }
+//   }, 1000);
+// };
+
+const formatTime = (t:number) => {
+  const min = Math.floor(t / 60);
+  const sec = t % 60;
+  return `${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`;
+};
+document.querySelector('.timer').innerHTML = formatTime(currentTime);
+const winCheck = () => {
+  if (guessedCards.length === fieldSize) {
+    console.log('WIN WIN WIN');
+    document.querySelector('.gameField').classList.add('hidden');
+    document.querySelector('.win').classList.remove('hidden');
+    clearInterval(time);
+    if (fieldSize === 6) {
+      points += 1;
+    } else if (fieldSize === 12) {
+      points += 3;
+    } else {
+      points += 5;
+    }
+    // gameStop = true;
+  }
+};
+const updateScore = () => {
+  document.querySelector('.points').innerHTML = `Score: ${points}`;
+};
+
+// And here starts game :)
 levelSelector.forEach((el, i) => {
   levelSelector[i].addEventListener('click', () => {
     fieldSize = (+levelSelector[i].value);
     const colSize = i === 0 ? 'col-4' : i === 1 ? 'col-3' : 'col-2';
+    // startTimer();
+    getRandomKeyNumbers();
     hideShow();
     drawFiled();
     getCards().forEach((card) => card.classList.add(colSize));
     cards = getCards();
-    getRandomkeys();
+    // gameStop = false;
 
     cards.forEach((card, ind) => {
       cards[ind].addEventListener('click', () => {
-        if (counter === 2) {
-          resetCardFaces();
-          counter = 0;
-          card1 = '';
-          card2 = '';
+        if (openedCards === 2) {
+          hideWrongGuess();
+          openedCards = 0;
+          firstOpenedCardValue = '';
+          secondOpenedCardValue = '';
         }
-        counter += 1;
-        cards[ind].innerHTML = `<img class="wrapper" src="${cardBacks[keys[randomkeys[ind]]]}">`;
-        if (counter === 1) {
-          card1 = keys[randomkeys[ind]];
-          card1Num = ind;
-        }
-        if (counter === 2) {
-          card2 = keys[randomkeys[ind]];
-          card2Num = ind;
-        }
-
-        if (card1 === card2) {
-          counter = 0;
-          guessedCards.push(card1Num);
-          guessedCards.push(card2Num);
-          card1 = '';
-          card2 = '';
+        if (firstOpenedCardIndex !== ind) {
+          openedCards += 1;
+          flipCard(ind);
+          if (openedCards === 1) {
+            firstOpenedCardValue = cardBackKeys[randomKeyNumbers[ind]];
+            firstOpenedCardIndex = ind;
+          }
+          if (openedCards === 2) {
+            secondOpenedCardValue = cardBackKeys[randomKeyNumbers[ind]];
+            secondOpenedCardIndex = ind;
+          }
+          if (firstOpenedCardValue === secondOpenedCardValue) {
+            openedCards = 0;
+            guessedCards.push(firstOpenedCardIndex);
+            guessedCards.push(secondOpenedCardIndex);
+            firstOpenedCardValue = '';
+            secondOpenedCardValue = '';
+          }
+          winCheck();
+          updateScore();
         }
       });
     });
