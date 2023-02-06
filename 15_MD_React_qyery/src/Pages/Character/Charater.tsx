c614de35952676a9d4c4a7aef566e163f5c5d739
edzusk <edzus.krisjanis@gmail.com>
2023-02-06 19:17:10 +0200

    15_MD_React_qyery first commit

--- 15_MD_React_qyery/src/Pages/Character/Charater.tsx
+++ 15_MD_React_qyery/src/Pages/Character/Charater.tsx
@@ -3,19 +3,18 @@ import axios from "axios";
 import { useParams } from "react-router-dom"
 import CardProps from "../../assets/CardProps";
 import Card from "../../components/Card/Card";
-type CharacterProps = {
-    
-}
+
 
 const Character = () => {
     const { id } = useParams();
 
-    const postQuery =  useQuery({
+    const postQuery =  useQuery<CardProps>({
         queryKey: ['Card'],
         queryFn: async () => {
-          const response = await axios.get<{results: CardProps[]}>(`https://rickandmortyapi.com/api/character/${id}`);
+          const response = await axios.get(`https://rickandmortyapi.com/api/character/${id}`);
           console.log(id)
-          return response.data.results;
+          console.log(response.data)
+          return response.data;
         }
       })
     
@@ -28,11 +27,13 @@ const Character = () => {
       }
     return (
         <div className="container">
-            {postQuery.data.map(card => (
+            <div>{JSON.stringify(postQuery.data.id)}</div>
+            <Card {...postQuery.data}/>
+            {/* {postQuery.data.map(card => (
           <div key={card.id} className="col-4">
             <Card key={card.id} {...card}/>
           </div>
-        ))}
+        ))} */}
         </div>
     )
 
